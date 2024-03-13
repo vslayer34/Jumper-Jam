e53f665eeb8bfb94de47ce77679ceb1eaf908e0d
vslayer34 <mohamd.elnagar.34@gmail.com>
2024-03-14 00:30:28 +0200

    Added horizontal movement to the character

---
+++ Scripts/Helper/Helper.cs
@@ -0,0 +1,10 @@
+using Godot;
+using System;
+
+
+namespace InputActionNames;
+public static class ActionName
+{
+	public const string MOVE_LEFT = "move_left";
+	public const string MOVE_RIGHT = "move_right";
+}
--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -1,11 +1,44 @@
 using Godot;
+using InputActionNames;
 using System;
 
 public partial class Player : CharacterBody2D
 {
 	[Export]
 	private float _speed;
-	
-	private float _direction;
+	private Vector2 _movmentDirection;
 
+
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+		MovePlayer();
+    }
+
+
+	/// <summary>
+	/// Take the input and set the direction of the movment
+	/// and steadly slow down the player to a halt when thers's no input
+	/// </summary>
+	private void MovePlayer()
+	{
+		float easeToHalt = 10.0f;
+		float direction = Input.GetAxis(ActionName.MOVE_LEFT, ActionName.MOVE_RIGHT);
+
+		if (direction != 0)
+		{
+			_movmentDirection = new Vector2(direction * _speed, Velocity.Y);
+			Velocity = _movmentDirection;
+		}
+		else
+		{
+			_movmentDirection = new Vector2(0.0f, Velocity.Y);
+			Velocity = Velocity.MoveToward(_movmentDirection, _speed / easeToHalt);
+		}
+
+		GD.Print(direction);
+		GD.Print(_movmentDirection);
+
+		MoveAndSlide();
+	}
 }
